254fa9feb5c44f2e87e6b6653d988e28dab4e4a5
Thomas Dorsch <t.dorsch@ibgndt.de>
2024-01-26 16:29:16 +0100

    Tmux: Use alt+arrow keys to navigate between panes. Nvim: Enable blinking cursor mode.

--- private_dot_config/nvim/lua/custom/plugins/init.lua
+++ private_dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+vim.o.guicursor = 'n-v:block,i:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
+return {
+}
