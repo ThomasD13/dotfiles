8f47adb848829430f353b5eda96c368a4579a7c1
Thomas Dorsch <t.dorsch@ibgndt.de>
2024-01-26 17:03:29 +0100

    Tmux: Adding hjkl binds to move bewteen panes again. Nvim: When leaving nvim, the previous cursor-style gets restored.

--- private_dot_config/nvim/lua/custom/plugins/init.lua
+++ private_dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,7 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set the blinking cursor settings
 vim.o.guicursor = 'n-v:block,i:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
+-- Make sure, that when we exit nvim the previous cursor settings get restored
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:block20
+    augroup END
+]])
 
 return {
 }
