e12f5e49a7e5eeb50e1283308e2ce17a90373094
Thomas Dorsch <t.dorsch@ibgndt.de>
2024-02-20 17:37:17 +0100

    Adding configuration for fzf 0.46.1 bash (file/path) autocompletion.

---
+++ private_dot_config/fzf/fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+# if [[ ! "$PATH" == */home/thomas/projects/fzf/bin* ]]; then
+#   PATH="${PATH:+${PATH}:}/home/thomas/projects/fzf/bin"
+# fi
+
+# Auto-completion
+# ---------------
+source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/completion.bash
+
+# Key bindings
+# ------------
+# source "/home/thomas/projects/fzf/shell/key-bindings.bash"
