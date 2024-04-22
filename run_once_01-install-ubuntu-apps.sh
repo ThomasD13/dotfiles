ad90e3e90ada2dddec0bfe946a18aa86f7e46fa8
Thomas Dorsch <t.dorsch@ibgndt.de>
2024-04-22 07:15:27 +0000

    Run_once Script: Install binary lazygit if not available.

--- run_once_01-install-ubuntu-apps.sh
+++ run_once_01-install-ubuntu-apps.sh
@@ -49,3 +49,14 @@ if [[ $? -ne 0 ]]; then
     wget https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_0.16.5_amd64.deb && dpkg -i git-delta_0.16.5_amd64.deb && rm git-delta_0.16.5_amd64.deb 
 fi 
 
+# Install lazygit as git command line interface
+command -v lazygit >/dev/null
+
+if [[ $? -ne 0 ]]; then
+    echo "lazygit is not installed"
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm lazygit
+fi 
